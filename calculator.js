91c2c56b5436127fc5dcca8da55cb38492ce7f45
salonikadam03 <salonikadam2303@gmail.com>
2024-12-09 10:22:18 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,21 @@
+function appendToDisplay(value) {
+  const display = document.getElementById('display');
+  display.value += value;
+}
+
+function clearDisplay() {
+  document.getElementById('display').value = '';
+}
+
+function calculate() {
+  const display = document.getElementById('display');
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    alert('Invalid calculation');
+    clearDisplay();
+  }
+
+  
+  
+}
